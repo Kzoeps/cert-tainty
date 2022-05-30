107d69ee92426aff21c9707cf4c9a855930cdeb7
kzoeps <kzoepa@gmail.com>
2022-05-30 23:18:05 +0600

    set up footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {ChakraProvider} from '@chakra-ui/react';
 import Navigation from '../components/navigation/navigation';
+import Footer from '../components/footer/footer';
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
 		<ChakraProvider>
 			<Navigation/>
 			<Component {...pageProps} />
+			<Footer/>
 		</ChakraProvider>
 	);
 }
