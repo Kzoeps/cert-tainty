9d993930decce7cd76910a583aea6b7fc6a4e9e6
kzoeps <kzoepa@gmail.com>
2022-06-04 23:42:35 +0600

    feat: set up apollo interceptor

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache, concat } from '@apollo/client';
+
+const httpLink = new HttpLink({ uri: process.env.NEXT_PUBLIC_RR_API });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+	// add the authorization to the headers
+	operation.setContext(({ headers = {} }) => ({
+		headers: {
+			...headers,
+			authorization: localStorage?.getItem('token') || null,
+		}
+	}));
+
+	return forward(operation);
+})
 
 const client = new ApolloClient({
-	uri: process.env.NEXT_PUBLIC_RR_API,
 	cache: new InMemoryCache(),
+	link: concat(authMiddleware, httpLink),
 });
 
 export default client;
