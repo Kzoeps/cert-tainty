d47e40c75aef1d2cb05d019ff8bba66ba6799603
kzoeps <kzoepa@gmail.com>
2022-06-07 00:28:04 +0600

    update stuff

---
+++ pages/mint-view/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export interface MintViewProps {
+}
+
+export const MintView = (props: MintViewProps) => {
+    return (
+        <>
+            <p> Hello World </p>
+        </>
+    )
+}
+
+export default MintView;
