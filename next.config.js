8e1e86e20fb4b0545f6fa5b9e57fd2cea8747c8d
kzoeps <kzoepa@gmail.com>
2022-06-07 16:28:06 +0600

    update: add domains on nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	images: {
+	  domains:['https://young-hollows-48335.herokuapp.com', 'https://lh3.googleusercontent.com/']
+	}
 }
 
 module.exports = nextConfig
--- pages/mint-view/[id].tsx
+++ pages/mint-view/[id].tsx
@@ -1,6 +1,7 @@
-import {Center, Flex, HStack, Stack} from '@chakra-ui/react';
+import {Box, Center, Flex, Heading, HStack, Stack} from '@chakra-ui/react';
 import React from "react";
 import {useRouter} from 'next/router';
+import Image from 'next/image';
 
 export interface MintViewProps {
 }
@@ -13,11 +14,13 @@ export const MintView = (props: MintViewProps) => {
 			<Center w="100%" bg="gray.50">
 					<Flex minH='100vh' align="center" justify="center" bg="gray.50">
 						<HStack w="100%" spacing={12}>
-							<Stack>
-								<p>{id}</p>
+							<Stack w="lg">
+								<Image layout="fill" alt="certificate" src="https://lh3.googleusercontent.com/lbBYOkRPvDaOZm2xBhsvTtwE9nubmUmL7FA5UjTDDZIXsxwpf5e5bjeUfhrNF95WNefnvoCLlwcWfhe6X_5_qRvR2ZIeYOIR7I5a=w600"/>
 							</Stack>
-							<Stack>
-								<p>Hello World</p>
+							<Stack rounded="lg" bg="white" as={Box} textAlign={'left'}>
+								<Heading as="h4" size="md">
+									Name
+								</Heading>
 							</Stack>
 						</HStack>
 					</Flex>
